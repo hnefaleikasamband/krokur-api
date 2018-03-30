783b7cffdfc660044b9c563a57fd766f9524f049
gdgunnars <gdg@gdg.is>
2018-03-30 18:42:40 +0200

    changed bcrypt-nodejs module to brcyptjs

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt-nodejs";
+import bcrypt from "bcryptjs";
 
 // TODO: change import to import { Schema } from "mongoose";
 const Schema = mongoose.Schema;
