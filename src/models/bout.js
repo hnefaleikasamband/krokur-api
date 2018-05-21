aa1c4d832332a8419a4c154b03015d38b95ed4ad
gdgunnars <gdg@gdg.is>
2018-05-21 12:39:39 +0200

    Updated club validation of schema

--- src/models/bout.js
+++ src/models/bout.js
@@ -16,7 +16,20 @@ const BoutSchema = new Schema({
         required: true
     },
     club: {
-        type: String
+        type: String,
+        validate: function(club) {
+            return new Promise( (resolve, reject) => {
+                Club.find({shorthand: club.toUpperCase()})
+                    .exec(function (err, clubs) {
+                        let valid = false;
+                        if (clubs && clubs.length > 0) {
+                            valid = true;
+                        }
+                        resolve(valid);
+                    });
+            });
+        },
+        message: 'Club not found'
     },
     type: {
         type: String,
