889154b5b990a63c9198ba9f82ea4dc6e28950ff
gdgunnars <gdg@gdg.is>
2018-09-02 00:16:15 +0200

    Added PUT & GET /athletes/:_id
    Created two routes, one to update athletes and another one to fetch a
    single athlete. When an athlete is either created og updated
    successfully the user is redirected to the GET route to get a consistent
    response.

--- src/router/athletes.js
+++ src/router/athletes.js
@@ -30,6 +30,24 @@ athleteRouter.get("/", (req, res, next) => {
 });
 
 /**
+ *
+ */
+athleteRouter.get("/:_id", async (req, res) => {
+  try {
+    const athlete = await Athlete.findById({ _id: req.params._id });
+    if (!athlete) {
+      return res.status(404).json();
+    }
+    res.json({ athlete });
+  } catch (error) {
+    console.log("Error in GET /api/v1/athlete/_id:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching athelte, check logs for details" });
+  }
+});
+
+/**
  * Creates a new athlete and saves it in mongo
  * POST /api/v1/athletes
  */
@@ -43,7 +61,7 @@ athleteRouter.post("/", async (req, res, next) => {
   try {
     const existingUser = await Athlete.findOne({ ssn: ssn });
     if (existingUser) {
-      return res.status(400).send({ message: "Athlete already exists" });
+      return res.status(400).json({ message: "Athlete already exists" });
     }
 
     let athlete = new Athlete({
@@ -56,7 +74,8 @@ athleteRouter.post("/", async (req, res, next) => {
     const validateResponse = await athlete.validate();
     console.log("validator response: ", validateResponse);
     await athlete.save();
-    return res.status(201).json({ athlete });
+    //return res.status(201).json({ athlete });
+    return res.status(200).redirect(`/api/v1/athletes/${athlete._id}`);
   } catch (error) {
     if (error.name === "ValidationError") {
       return res.status(400).json({ message: error.message });
@@ -66,6 +85,38 @@ athleteRouter.post("/", async (req, res, next) => {
         .status(500)
         .json({ message: "Error saving athlete, check logs for details" });
     }
+  }
+});
+
+/**
+ * Updates a single athlete based on the ID given
+ * PUT /api/v1/athlete/
+ */
+athleteRouter.put("/:_id", async (req, res) => {
+  try {
+    console.log("The id given:", req.params._id);
+    const athlete = await Athlete.findById({ _id: req.params._id });
+    console.log("printing athlete:", athlete);
+    if (!athlete) {
+      return res.status(404).json();
+    }
+    const name = req.body.name ? req.body.name : athlete.name;
+    const ssn = req.body.ssn ? kennitala.clean(req.body.ssn) : athlete.ssn;
+    const club = req.body.club ? req.body.club : athlete.club;
+
+    Object.assign(athlete, { name }, { ssn }, { club });
+    await athlete.validate();
+    await athlete.save();
+    // We need to issue a 303 redirect to allow the browser to accept PUT -> GET conversion.
+    return res.status(200).redirect(303, `/api/v1/athletes/${req.params._id}`);
+  } catch (error) {
+    console.log("Error in PUT /athletes/:_id :", error);
+    if (error.name === "ValidationError") {
+      return res.status(400).json(error.message);
+    }
+    return res
+      .status(500)
+      .json({ message: "Error updating athlete, check logs for details" });
   }
 });
 
