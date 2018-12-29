82bd79bee57dfa7fd7239da912af9212f22cd135
Gunnar Davíð <gdg@trustpilot.com>
2018-12-29 20:49:31 +0100

    trying out new deploy methods

---
+++ scripts/deploy-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "--- Deploy script is loading ---"
+echo "Checking if TRAVIS_TAG is present: "
+echo $TRAVIS_TAG
+if [[ ! $TRAVIS_TAG ]]; 
+then
+  URI=$PRODUCTION_URI
+  PORT="22"
+else
+  URI=$STAGING_URI
+  PORT="22022"
+fi
+
+echo "Printing out $URI & $PORT"
+echo $URI
+echo $PORT
+
+PROJECT_FOLDER="krokur-api"
+IMAGE_ARCHIVE_NAME="krokur-api.tar"
+
+# Move the archived image to server
+rsync -rv --delete-after --progress -e "ssh -p 22022" $TRAVIS_BUILD_DIR/krokur-api.tar travis@$URI:~/$PROJECT_FOLDER/$IMAGE_ARCHIVE_NAME
+pwd
+ls -al
+rsync -rv --delete-after --progress -e "ssh -p 22022" $TRAVIS_BUILD_DIR/docker-compose.yml travis@$URI:~/$PROJECT_FOLDER/docker-compose.yml
+
+# SSH in to server & run the launch-script
+ssh travis@$URI -p $PORT 'bash -s' < launch-script.sh
+
+echo "--- Deploy script has finished loading ---"
