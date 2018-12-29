9d50b838284a8bffebb0078215c06caa6709f12d
Gunnar Davíð <gdg@trustpilot.com>
2018-12-29 21:38:51 +0100

    fixing launch issues

--- scripts/deploy-script.sh
+++ scripts/deploy-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "--- Deploy script is loading ---"
-printf "Checking if TRAVIS_TAG is present: %s \n" "$TRAVIS_TAG"
+printf "- Checking if TRAVIS_TAG is present: %s \n" "$TRAVIS_TAG"
 if [[ -z "$TRAVIS_TAG" ]]; 
 then
   # Variables is empty or not present
@@ -15,13 +15,13 @@ PROJECT_FOLDER="krokur-api"
 IMAGE_ARCHIVE_NAME="krokur-api.tar"
 
 # Move the archived image to server
-printf "Moving %s to \n" "$IMAGE_ARCHIVE_NAME"
+printf "- Moving %s to \n" "$IMAGE_ARCHIVE_NAME"
 rsync -rv --delete-after --progress -e "ssh -p 22022" $TRAVIS_BUILD_DIR/$IMAGE_ARCHIVE_NAME travis@$URI:~/$PROJECT_FOLDER/$IMAGE_ARCHIVE_NAME
-printf "Copy docker-compose.yml over to server\n"
+printf "- Copy docker-compose.yml over to server\n"
 rsync -rv --delete-after --progress -e "ssh -p 22022" $TRAVIS_BUILD_DIR/docker-compose.yml travis@$URI:~/$PROJECT_FOLDER/docker-compose.yml
 
 # SSH in to server & run the launch-script
-printf "SSH into server & run launch-script.sh"
+printf "- SSH into server & run launch-script.sh\n"
 ssh travis@$URI -p $PORT 'bash -s' < scripts/launch-script.sh
 
 echo "--- Deploy script has finished loading ---"
--- scripts/launch-script.sh
+++ scripts/launch-script.sh
@@ -4,13 +4,15 @@ PROJECT_FOLDER="krokur-api"
 IMAGE_ARCHIVE_NAME="krokur-api.tar"
 
 pushd $PROJECT_FOLDER
+echo "- Loading image to docker"
 docker load -i $IMAGE_ARCHIVE_NAME
 
 # -p is for "no error if existing, make parent directories as needed"
 # then run docker-compose up with --build to use the new image previously loaded.
-mkdir -p ./db; docker-compose up -d --build --rm=true
+mkdir -p ./db; docker-compose up -d --build
 
 # Remove the archived image
+printf "- Removing %s from server\n" "$IMAGE_ARCHIVE_NAME"
 rm $IMAGE_ARCHIVE_NAME
 
 # Check if we have a dangling <none> images and remove them if so
