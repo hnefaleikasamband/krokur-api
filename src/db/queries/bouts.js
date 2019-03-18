8925ae513635b1e91b6c16e4db9fd975c6210913
gdg <>
2019-03-18 22:32:51 +0100

    updating correct mappin & minor code tweaks

--- src/db/queries/bouts.js
+++ src/db/queries/bouts.js
@@ -2,7 +2,7 @@ const getBoutById = async (db, id) => db.any('SELECT * from bouts where id = ${i
 
 const getAllBoutsForAthlete = async (db, athleteId) => db.any('SELECT * from bouts where athlete_id = ${athleteId}', { athleteId });
 
-const addBout = async (db, bout) => db.any(
+const addBout = async (db, bout) => db.one(
   'INSERT INTO bouts (athlete_id, athlete_name, athlete_club_shorthand, opponent_id, opponent_name, '
       + 'opponent_club_shorthand, type, bout_date, points, organizer) '
       + 'VALUES(${athleteId}, ${athleteName}, ${athleteClubShortHand}, ${opponentId}, ${opponentName}, '
