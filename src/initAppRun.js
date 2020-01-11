b2a45e7d9dacaaa50a6e5f65e7b62d7ecc93bd40
GDGunnars <4069821+gdgunnars@users.noreply.github.com>
2020-01-11 13:38:31 +0100

    Update init script to include athlete Org & Anon

--- src/initAppRun.js
+++ src/initAppRun.js
@@ -1,8 +1,13 @@
 import Joi from 'joi';
-import { usersQueries } from './db/index';
+import { usersQueries, clubsQueries, athletesQueries } from './db/index';
 import schema from './db/schemas';
 import utils from './services/utils';
 import logger from './config/logger';
+
+const ATHLETE_ORGANIZATION = {
+  name: 'Hnefaleikasamband Íslands',
+  shorthand: 'HNÍ',
+};
 
 async function addSuperUser(dbConn) {
   const user = {
@@ -25,20 +30,68 @@ async function addSuperUser(dbConn) {
 
   const newUser = await usersQueries.addUser(dbConn, validatedUser);
   logger.info(
-    `Inserted Super User:\n\tname(email): ${newUser.name}(${newUser.email}),\n\trole: ${
-      newUser.role
-    }\n\tdisabled: ${newUser.disabled}`,
+    `Inserted Super User -> name(email): ${newUser.name}(${newUser.email}), role: ${newUser.role} disabled: ${newUser.disabled}`,
+  );
+}
+
+
+async function addOrgClub(dbConn) {
+  const validatedClub = await Joi.validate(
+    ATHLETE_ORGANIZATION,
+    schema.clubSchema,
+    schema.defaultValidationOptions,
+  );
+
+  const newClub = await clubsQueries.addClub(dbConn, validatedClub);
+  logger.info(
+    `Inserted Organization as Club -> name(shorthand): ${newClub.name}(${newClub.shorthand})`,
+  );
+  return newClub;
+}
+
+async function addAnonAthlete(dbConn, club) {
+  const athlete = {
+    name: 'Anonymous',
+    ssn: '0000000000',
+    club: club.id,
+  };
+
+  const validatedAthlete = await Joi.validate(
+    athlete,
+    schema.athleteSchema,
+    schema.defaultValidationOptions,
+  );
+
+  console.log(validatedAthlete);
+
+  const newAthlete = await athletesQueries.addAthlete(dbConn, validatedAthlete);
+  logger.info(
+    `Inserted Anonymous athlete -> name: ${newAthlete.name}, ssn: ${newAthlete.ssn} club: ${club.shorthand}`,
   );
 }
 
 const init = async (dbConn) => {
   try {
-    logger.info('... checking for super-user');
-    const users = await usersQueries.getAllUsers(dbConn);
+    logger.info('... Init checks starting');
+    const usersQuery = usersQueries.getAllUsers(dbConn);
+    const clubsQuery = clubsQueries.getAllClubs(dbConn);
+    const anonExistsQuery = athletesQueries.athleteExists(dbConn, '0000000000');
+    const [users, clubs, anonExists] = await Promise.all([usersQuery, clubsQuery, anonExistsQuery]);
+
+    if (!anonExists) {
+      const athleteOrgExists = clubs.find(c => c.shorthand === ATHLETE_ORGANIZATION.shorthand);
+      console.log(athleteOrgExists);
+      const athleteOrg = athleteOrgExists || await addOrgClub(dbConn);
+      await addAnonAthlete(dbConn, athleteOrg);
+    } else {
+      logger.info('... Anon user & Athlete Org already registered, no action needed!');
+    }
+
     if (users && users.length > 0) {
       logger.info('... There are some users already in the database, super-user was not added!');
       return;
     }
+
     await addSuperUser(dbConn);
     return;
   } catch (error) {
